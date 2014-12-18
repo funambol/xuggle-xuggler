f0d7775453c98181bb91f2b5b86dc7161a241023
Richard Titze <richard.titze@gmail.com>
2014-12-18 16:17:31 +0100

    fixes for potential segmentation faults

--- captive/ffmpeg/csrc/libavcodec/avpacket.c
+++ captive/ffmpeg/csrc/libavcodec/avpacket.c
@@ -36,7 +36,9 @@ void ff_packet_free_side_data(AVPacket *pkt)
     int i;
     for (i = 0; i < pkt->side_data_elems; i++)
         av_free(pkt->side_data[i].data);
-    av_freep(&pkt->side_data);
+    if (pkt->side_data) {
+        av_freep(&pkt->side_data);
+    }
     pkt->side_data_elems = 0;
 }
 
--- captive/ffmpeg/csrc/libavutil/mem.c
+++ captive/ffmpeg/csrc/libavutil/mem.c
@@ -176,9 +176,11 @@ void av_free(void *ptr)
 
 void av_freep(void *arg)
 {
-    void **ptr= (void**)arg;
-    av_free(*ptr);
-    *ptr = NULL;
+    if (arg) {
+        void **ptr= (void**)arg;
+        av_free(*ptr);
+        *ptr = NULL;
+    }
 }
 
 void *av_mallocz(size_t size)
